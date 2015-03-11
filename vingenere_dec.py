fd2986164ef3216360f8a2094df41d2483f773a4
Jocelin <jlee0523@gmail.com>
2015-03-11 22:35:47 +0100

    setup

---
+++ vingenere_dec.py
@@ -0,0 +1,14 @@
+# VINGENERE CYPHER DECRYPTER
+# by: Jocelin Lee
+
+# Master function for finding the length of the key.
+def findKeyLength():
+
+
+
+def main():
+    print("hello world!");
+
+
+if __name__ == "__main__":
+    main()
